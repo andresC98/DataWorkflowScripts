ef923ee178542d93a175ff991ff478699a4177ec
Andres Carrillo Lopez <acarrillolopez98@gmail.com>
2020-09-15 15:19:14 +0200

    solved bug with vizkmeans3dscatter function

--- visualization_functions.py
+++ visualization_functions.py
@@ -1,4 +1,6 @@
-def viz_kmeans_3DScatter(col_names, 3D_data, kmeansModel):
+import matplotlib.pyplot as plt
+
+def viz_kmeans_3DScatter(col_names, data, kmeansModel):
     '''
     Given an array of 3 different column names for a 3D data array, produces a 
     3D ScatterPlot with the kmeans resulting colored clusters.
@@ -7,9 +9,9 @@ def viz_kmeans_3DScatter(col_names, 3D_data, kmeansModel):
     t = fig.suptitle(col_names[0] + '-' + col_names[1] + '-' + col_names[2] , fontsize=14)
     ax = fig.add_subplot(111, projection='3d')
 
-    xs = list(3D_data[:,0])
-    ys = list(3D_data[:,1])
-    zs = list(3D_data[:,3])
+    xs = list(data[:,0])
+    ys = list(data[:,1])
+    zs = list(data[:,3])
 
     ax.scatter(xs, ys, zs, s=50, alpha=0.6, edgecolors='w',c=kmeansModel.labels_.astype(float))
 
